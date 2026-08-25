4cac096c25e87cd79b939f5baed76b52a227245a
Peter Permenter <41281403+TusanHomichi@users.noreply.github.com>
2026-08-25 06:28:21 +0200

    ci(parity): tolerate slow Arch archive downloads

--- scripts/test-release-matrix.sh
+++ scripts/test-release-matrix.sh
@@ -1165,6 +1165,21 @@ test_check_release_matrix_requires_hosted_alpm_parity_producer() {
     done
 }
 
+test_check_release_matrix_requires_resilient_alpm_archive_downloads() {
+    local repo
+    local workflow
+
+    for workflow in pr-gate.yml merge-validation.yml; do
+        repo="$(create_release_policy_fixture)"
+        replace_fixture_text_once \
+            "$repo/.github/workflows/$workflow" \
+            "          sed -i '/^\[options\]/a DisableDownloadTimeout' /etc/pacman.conf" \
+            '          true'
+
+        assert_check_release_matrix_fails "$repo" "hosted"
+    done
+}
+
 test_check_release_matrix_requires_hosted_rpm_parity_producer() {
     local repo
     local workflow
@@ -1490,6 +1505,7 @@ main() {
         test_check_release_matrix_requires_shared_namespace_setup_in_every_workspace_lane
         test_check_release_matrix_rejects_unproven_namespace_action
         test_check_release_matrix_requires_hosted_alpm_parity_producer
+        test_check_release_matrix_requires_resilient_alpm_archive_downloads
         test_check_release_matrix_requires_hosted_rpm_parity_producer
         test_check_release_matrix_requires_hosted_debian_parity_producer
         test_check_release_matrix_rejects_namespace_setup_after_workspace_tests
